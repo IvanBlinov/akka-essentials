10e4aa789d3b104290acaa89bf8aeecf3b9bc05e
ivan-blinov-exa <ivanblinov@exabeam.com>
2019-10-02 12:28:49 +0300

    TestProbeSpec

--- src/main/scala/part3testing/TestProbeSpec.scala
+++ src/main/scala/part3testing/TestProbeSpec.scala
@@ -1,7 +1,7 @@
 package part3testing
 
-import akka.actor.ActorSystem
-import akka.testkit.{ImplicitSender, TestKit}
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import akka.testkit.{ImplicitSender, TestKit, TestProbe}
 import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
 
 class TestProbeSpec extends TestKit(ActorSystem("TestProbeSpec"))
@@ -11,9 +11,81 @@ class TestProbeSpec extends TestKit(ActorSystem("TestProbeSpec"))
     TestKit.shutdownActorSystem(system)
   }
 
+  import TestProbeSpec._
 
+  "A master actor" should {
+    "register a slave" in {
+      val master = system.actorOf(Props[Master])
+      val slave = TestProbe("slave")
+
+      master ! Register(slave.ref)
+      expectMsg(RegistrationAck)
+    }
+
+    "send the work to the slave actor" in {
+      val master = system.actorOf(Props[Master])
+      val slave = TestProbe("slave")
+
+      master ! Register(slave.ref)
+      expectMsg(RegistrationAck)
+
+      val workloadString = "I love Akka"
+      master ! Work(workloadString)
+
+      slave.expectMsg(SlaveWork(workloadString, testActor))
+      slave.reply(WorkCompleted(3, testActor))
+
+      expectMsg(Report(3))
+    }
+
+    "aggregate data correctly" in {
+      val master = system.actorOf(Props[Master])
+      val slave = TestProbe("slave")
+
+      master ! Register(slave.ref)
+      expectMsg(RegistrationAck)
+
+      val workloadString = "I love Akka"
+      master ! Work(workloadString)
+      master ! Work(workloadString)
+
+      slave.receiveWhile() {
+        case SlaveWork(`workloadString`, `testActor`) => slave.reply(WorkCompleted(3, testActor))
+      }
+
+      expectMsg(Report(3))
+      expectMsg(Report(6))
+    }
+  }
 }
 
 object TestProbeSpec {
-  // Word counting actor hierarchy master
+  // Word counting actor hierarchy master-slave
+  case class Register(slaveRef: ActorRef)
+  case class SlaveWork(text: String, originalRequester: ActorRef)
+  case class Work(text: String)
+  case class WorkCompleted(count: Int, originalRequester: ActorRef)
+  case class Report(totalWordCount: Int)
+  case object RegistrationAck
+
+  class Master extends Actor {
+    override def receive: Receive = {
+      case Register(slaveRef: ActorRef) =>
+        sender() ! RegistrationAck
+        context.become(online(slaveRef, 0))
+      case _ => // ignore
+    }
+
+    def online(slaveRef: ActorRef, totalWordCount: Int): Receive = {
+      case Work(text) => slaveRef ! SlaveWork(text, sender())
+      case WorkCompleted(count, originalRequester) =>
+        val newTotalWordCount = totalWordCount + count
+        originalRequester ! Report(newTotalWordCount)
+        context.become(online(slaveRef, newTotalWordCount))
+    }
+  }
+
+  /*class Slave extends Actor {
+
+  }*/
 }
